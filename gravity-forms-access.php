c5fc452141b0b25d9a0bb364ee9b230c2da63716
Asfand Yar Ali Khan <asfandkhan404@gmail.com>
2022-04-13 16:04:23 +0500

    User Role & Capabilities finalized

--- gravity-forms-access.php
+++ gravity-forms-access.php
@@ -11,21 +11,33 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 
-class Gravity_Forms_Access {
+if ( ! class_exists( 'Gravity_Forms_Access' )) :
+    class Gravity_Forms_Access {
 
-    function register_hooks() {
-        add_action( 'init', [$this, 'create_user_role'] );
+        function register_hooks() {
+            add_action( 'init', [$this, 'create_user_role'] );
+        }
+
+        /**
+         * Creates Role in WordPress Core
+         *
+         * @return void
+         */
+        function create_user_role() {
+
+            $capabilities = array(
+                'edit_dashboard'            => true,
+                'manage_links'              => true,
+                'read'                      => true,
+                'gravityforms_view_entries' => true
+            );
+
+            add_role( 'viewer', 'Viewer', $capabilities );
+        }
+
     }
 
-    function create_user_role() {
-        $capabilities = array(
-            'read'  => true
-        );
+    $gravity_forms_access = new Gravity_Forms_Access();
+    $gravity_forms_access->register_hooks();
 
-        add_role( 'viewer', 'Viewer', $capabilities );
-    }
-
-}
-
-$gravity_forms_access = new Gravity_Forms_Access();
-$gravity_forms_access->register_hooks();
+endif;
