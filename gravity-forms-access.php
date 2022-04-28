a72e632f91a42ff4ed1c190d0e7c9af16a945527
Asfand Yar Ali Khan <asfandkhan404@gmail.com>
2022-04-28 18:47:33 +0500

    Refactored and added a new user role

--- gravity-forms-access.php
+++ gravity-forms-access.php
@@ -7,16 +7,19 @@
 * Author URI: http://www.640square.com/
 */
 
-if ( !defined ( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 
-if ( ! class_exists( 'Gravity_Forms_Access' )) :
-    class Gravity_Forms_Access {
+if (!class_exists('Gravity_Forms_Access')) :
+    class Gravity_Forms_Access
+    {
 
-        function initialize_hook() {
-            add_action( 'init', [ $this, 'create_user_role' ] );
-            add_action( 'init', [ $this, 'load_form_view' ] );
+        function initialize_hook()
+        {
+
+            add_action('init', [$this, 'create_user_role']);
+            add_action('init', [$this, 'load_form_view']);
         }
 
         /**
@@ -24,7 +27,8 @@ if ( ! class_exists( 'Gravity_Forms_Access' )) :
          *
          * @return void
          */
-        function create_user_role() {
+        function create_user_role()
+        {
 
             $capabilities = array(
                 'edit_dashboard'            => true,
@@ -33,19 +37,47 @@ if ( ! class_exists( 'Gravity_Forms_Access' )) :
                 'gravityforms_view_entries' => true
             );
 
-            add_role( 'Viewer', 'Viewer', $capabilities );
-            add_role( 'Argenta Flats - North Little Rock, AR', 'Argenta Flats - North Little Rock, AR', $capabilities );
-            add_role( 'Brentwood Apartments – Conway. AR', 'Brentwood Apartments – Conway. AR', $capabilities );
-            add_role( 'Glenrock Apartments – Conway, AR', 'Glenrock Apartments – Conway, AR', $capabilities );
-            add_role( 'Helen Street – Conway, AR', 'Helen Street – Conway, AR', $capabilities );
-            add_role( 'Jlofts – Conway, AR', 'Jlofts – Conway, AR', $capabilities );
-            add_role( 'Kyle Kourt – Conway, AR', 'Kyle Kourt – Conway, AR', $capabilities );
-            add_role( 'Lasley Historic Flats – Conway, AR', 'Lasley Historic Flats – Conway, AR', $capabilities );
-            add_role( 'Robinson Court – Conway, AR', 'Robinson Court – Conway, AR', $capabilities );
-            add_role( 'Row Houses at Hendrix Village – Conway, AR', 'Row Houses at Hendrix Village – Conway, AR', $capabilities );
-            add_role( 'Robins Square Apartments – Conway, AR', 'Robins Square Apartments – Conway, AR', $capabilities );
-            add_role( 'St. James Park – Conway, AR', 'St. James Park – Conway, AR', $capabilities );
-            add_role( 'Centerstone - Conway, AR', 'Centerstone - Conway, AR', $capabilities );
+            add_role('Viewer', 'Viewer', $capabilities);
+            add_role('Argenta Flats - North Little Rock, AR', 'Argenta Flats - North Little Rock, AR', $capabilities);
+            add_role('Brentwood Apartments – Conway. AR', 'Brentwood Apartments – Conway. AR', $capabilities);
+            add_role('Glenrock Apartments – Conway, AR', 'Glenrock Apartments – Conway, AR', $capabilities);
+            add_role('Helen Street – Conway, AR', 'Helen Street – Conway, AR', $capabilities);
+            add_role('Jlofts – Conway, AR', 'Jlofts – Conway, AR', $capabilities);
+            add_role('Kyle Kourt – Conway, AR', 'Kyle Kourt – Conway, AR', $capabilities);
+            add_role('Lasley Historic Flats – Conway, AR', 'Lasley Historic Flats – Conway, AR', $capabilities);
+            add_role('Robinson Court – Conway, AR', 'Robinson Court – Conway, AR', $capabilities);
+            add_role('Row Houses at Hendrix Village – Conway, AR', 'Row Houses at Hendrix Village – Conway, AR', $capabilities);
+            add_role('Robins Square Apartments – Conway, AR', 'Robins Square Apartments – Conway, AR', $capabilities);
+            add_role('St. James Park – Conway, AR', 'St. James Park – Conway, AR', $capabilities);
+            add_role('Centerstone - Conway, AR', 'Centerstone - Conway, AR', $capabilities);
+            add_role('ONLINEAPP-ENGAGE', 'ONLINEAPP-ENGAGE', $capabilities);
+        }
+
+        function return_gf_search()
+        {
+            if ($this->get_current_user_role() == 'ONLINEAPP-ENGAGE') {
+                $search = array(
+                    'St. James Park – Conway, AR',
+                    'Robins Square Apartments – Conway, AR',
+                    'Row Houses at Hendrix Village – Conway, AR',
+                    'Robinson Court – Conway, AR',
+                    'Lasley Historic Flats – Conway, AR',
+                    'Kyle Kourt – Conway, AR',
+                    'Jlofts – Conway, AR',
+                    'Helen Street – Conway, AR'
+                );
+                return array(
+                    'key'   => '23',
+                    'operator' => 'in',
+                    'value' => $search
+                );
+
+            } else {
+                return array(
+                    'key'   => '23',
+                    'value' => $this->get_current_user_role()
+                );
+            }
         }
 
         /**
@@ -60,9 +92,11 @@ if ( ! class_exists( 'Gravity_Forms_Access' )) :
                 'status'        => 'active',
                 'field_filters' => array(
                     'mode' => 'any',
+                    $this->return_gf_search(),
                     array(
-                        'key'   => '2',
-                        'value' => $this->get_current_user_role()
+                        'key'   => '23',
+                        'operator' => 'in',
+                        'value' => $this->return_gf_search()
                     ),
                 )
             );
@@ -75,26 +109,53 @@ if ( ! class_exists( 'Gravity_Forms_Access' )) :
          *
          * @return Current_User_Role
          */
-        function get_current_user_role() {
+        function get_current_user_role()
+        {
 
             // Get current User Role
             $user = wp_get_current_user()->roles[0];
             return $user;
-
         }
+
+        function redirect_to_form()
+        {
+            $url = admin_url("admin.php?page=gf_entries&id=9");
+            wp_redirect($url);
+        }
+
+        function remove_menu_pages_gf()
+        {
+            remove_menu_page('index.php');
+            remove_menu_page('profile.php');
+            remove_menu_page('users.php');
+            remove_menu_page('esign-ninja-about');
+            remove_menu_page('vc-welcome');
+        }
+
+        function remove_gf_css()
+        {
+            echo "<script> document.querySelector('.gform-form-toolbar').style.display = 'none'; </script>";
+            echo "<script> document.querySelector('#entry_search_container').style.display = 'none'; </script>";
+        }
+
 
         /**
          * Loads the hook for specific Role
          *
          * @return void
          */
-        function load_form_view() {
+        function load_form_view()
+        {
 
             $user = $this->get_current_user_role();
 
-            if ( $user !== 'administrator' ) {
-                add_filter( 'gform_search_criteria_entry_list_1', [ $this, 'get_required_form_entries' ], 10, 2 );
-            } else if ( $user == 'administrator' ) {
+            if ($user !== 'administrator') {
+                add_action('admin_init', [$this, 'remove_menu_pages_gf']);
+                add_filter('gform_search_criteria_entry_list_9', [$this, 'get_required_form_entries'], 10, 2);
+                add_action('profile_personal_options', [$this, 'redirect_to_form']);
+                add_action('admin_init', [$this, 'remove_gf_css']);
+            } else if ($user == 'administrator') {
+
                 return;
             }
         }
